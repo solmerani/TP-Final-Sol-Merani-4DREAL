d203374fd399503c33e71e6d21df5850c66112d7
solmerani <148452997+solmerani@users.noreply.github.com>
2024-06-24 09:18:04 -0300

    edesfdsf

---
+++ Assets/ObjetosScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetosScript : MonoBehaviour
+{
+    public float botonIzquierdo;
+    public float botonDerecho;
+    public int currentIndex = 0;
+    public GameObject[] assets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ShowPreviousAsset();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ShowNextAsset();
+        }
+
+        
+    }
+    void ShowNextAsset()
+    {
+        assets[currentIndex].SetActive(false);
+     currentIndex =   (currentIndex + 1) % assets.Length;
+        assets[currentIndex].SetActive(true);
+
+
+    }
+
+    void ShowPreviousAsset()
+    {
+        assets[currentIndex].SetActive(false);
+        currentIndex = (currentIndex - 1) % assets.Length;
+        assets[currentIndex].SetActive(true);
+    }
+
+}
