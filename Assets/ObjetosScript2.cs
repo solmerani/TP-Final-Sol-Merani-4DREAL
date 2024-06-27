b0ac15cf5a39c10fad4f8f48820219763c00f926
solmerani <148452997+solmerani@users.noreply.github.com>
2024-06-27 11:16:00 -0300

    ANDA!!

---
+++ Assets/ObjetosScript2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetosScript2 : MonoBehaviour
+{
+    public GameObject[] objetos; // Arreglo de objetos
+    public int objetosIndex = 0; // Índice del objeto activo
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ActivateNextLight();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ActivatePreviousLight();
+        }
+    }
+
+    public void ActivateNextLight()
+    {
+        objetosIndex++;
+        if (objetosIndex >= objetos.Length)
+        {
+            objetosIndex = 0;
+        }
+        DeactivateAllLights();
+        objetos[objetosIndex].SetActive(true);
+    }
+
+    public void ActivatePreviousLight()
+    {
+        objetosIndex--;
+        if (objetosIndex < 0)
+        {
+            objetosIndex = objetos.Length - 1;
+        }
+        DeactivateAllLights();
+        objetos[objetosIndex].SetActive(true);
+    }
+
+    void DeactivateAllLights()
+    {
+        foreach (GameObject g in objetos)
+        {
+            g.SetActive(false);
+        }
+    }
+
+
+    public void ActivateRepeating(float t)
+    {
+
+    }
+}
+
+
+
